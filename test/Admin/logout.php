fc30239d9f5c6112c54f202b9434b3f551552dc2
badalkotak <badalkotak@gmail.com>
2016-05-01 10:51:36 +0530

    Deleted Duplicate Directory

--- test/Admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-session_start();
-unset($_SESSION['uname']);
-session_destroy();
-header('Location:index.php');
-?>
--- test/Researcher/dbcon.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-if($con=mysqli_connect("localhost","iitsus","1qazxsw2","iitsus"))
-{}
-else
-{echo "Problem in connecting to Database";}
-?>
